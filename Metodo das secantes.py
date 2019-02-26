c8264d76e9fac613808f7d0e1e79c2a1bd330df0
Henrique Parucker <henriquewps@gmail.com>
2019-02-26 20:21:04 -0300

    Add files via upload

--- Metodo das secantes.py
+++ Metodo das secantes.py
@@ -1,34 +1,33 @@
+#Autor : Henrique Wippel Parucker da Silva
 from sympy import *
 x = Symbol('x')
 if __name__ == '__main__':
     a = input("Digite o A do intervalo para a função f(x) = x²-2:  ")
     b = input("Digite o B do intervalo para a função f(x) = x²-2:  ")
     f = "x**2 - 2"
-    fx = sympify(f)
-    fa = fx.evalf(subs={x : a})
-    fb = fx.evalf(subs={x : b})
+    fx = sympify(f) #Transforma string em uma função
+    fa = fx.evalf(subs={x : a}) #Substitui o intervalo A na função e resolve ela
+    fb = fx.evalf(subs={x : b}) #Substitui o intervalo B na função e resolve ela
     p1 = a
     p2 = b
     v = input("Digite o numero de interações: ")
-
     pn = a
     pn1 = b
 
     print("-----------------------")
-    for n in range (0,int(v)):
+    for n in range (0,int(v)): #Looping para resolver o número de interações digitado pelo usuário
         print ("n = ", n+1)
         print ("pn =", pn)
-        #pn1 = int(pn)+1
         print ("pn+1 = ", pn1)
-        fpn = fx.evalf(subs={x:pn})
+        fpn = fx.evalf(subs={x:pn}) #Resolve f(pn)
         print ("f(pn) = ", fpn)
-        fpn1 = fx.evalf(subs={x:pn1})
+        fpn1 = fx.evalf(subs={x:pn1}) #Resolve f(pn+1)
         print("f(pn+1) = ", fpn1)
         a = fpn1 - fpn
         b = float(pn1) - float(pn)
         c = fpn*b
-        pn2= float(pn)- c/a
-        print ("pn+2=", (pn2))
+        pn2= float(pn)- c/a #Resolve pn+2 = pn - (f(pn)*(pn+1 - pn))/(f(pn+1)-f(pn))
+        print ("pn+2 =", (pn2))
         print ("-----------------------")
         pn = pn1
         pn1 = pn2
